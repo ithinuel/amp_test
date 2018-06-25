78dd5d5b5d5882e76df964cb657e164ab522a70e
Wilfried Chauveau <wilfried.chauveau@arm.com>
2018-06-25 16:49:54 +0100

    second commit

---
+++ Readme.txt
@@ -0,0 +1,1 @@
+This explores the capabilities of gcc to build & link for different arm cores into the same binary output.
--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ else
 fi
 
 arm-none-eabi-gcc $@ -Os -mcpu=cortex-m0 -c test_cm0.c -o cm0.o || exit
-arm-none-eabi-gcc $@ -Os -mcpu=cortex-m0 -c main.c     -o m.o || exit
+arm-none-eabi-gcc $@ -Os -mcpu=cortex-m3 -c main.c     -o m.o || exit
 arm-none-eabi-gcc $@ -Os -mcpu=cortex-m3 -c test_cm3.c -o cm3.o || exit
 arm-none-eabi-gcc $@ -Os -nostartfiles -mcpu=$LINKMCPU -o multicore.elf cm0.o cm3.o m.o || exit
-arm-none-eabi-objdump -DSC multicore.elf | pygmentize
+arm-none-eabi-objdump -DSC multicore.elf | pygmentize | less
